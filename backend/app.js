c0862b64d9254035526b33429673abc84506e39f
NikhileshP <nikhileshhpandey@gmail.com>
2020-09-10 22:13:58 +0530

    🚧 creating server file - Handling errors using Express js

--- backend/app.js
+++ backend/app.js
@@ -54,3 +54,10 @@ app.get('/', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/Record-Management'));
 });
+
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.message);
+  if (!err.statusCode) err.statusCode = 500;
+  res.status(err.statusCode).send(err.message);
+});
