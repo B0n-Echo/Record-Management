a6407d516e3799f17d7cf1cd5631479eeb477641
NikhileshP <nikhileshhpandey@gmail.com>
2020-09-10 22:09:26 +0530

    🚧 Setup port using express

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ let cors= require('cors');
 let bodyParser= require('body-parser');
 let dataBaseConfig  = require('./database/db.js');
 const studentRoute = require('./routes/student.route');
+const app = express();
 
 // MongoDb Connection configuration
 mongoose.Promise = global.Promise;
@@ -20,3 +21,18 @@ error => {
 
 // Set up express js port
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(cors());
+
+// Setting up static director
+app.use(express.static(path.join(__dirname + 'dist/Record-Management')));
+
+
+// PORT
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log('Connected to port ' + port)
+});
