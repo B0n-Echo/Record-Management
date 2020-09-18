47cb8fb6d16579eb279784fda152962114e61d53
NikhileshP <nikhileshhpandey@gmail.com>
2020-09-18 11:52:57 +0530

    🏗 Add Student - added Template and UI changes

--- src/app/components/add-student/add-student.component.ts
+++ src/app/components/add-student/add-student.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, ViewChild, NgZone } from '@angular/core';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { MatChipInputEvent } from '@angular/material/chips';
 import { ApiService } from './../../shared/api.service';
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 export interface Subject {
   name: string;
@@ -52,7 +52,7 @@ export class AddStudentComponent implements OnInit {
       subjects: [this.subjectArray],
       dob: ['', [Validators.required]],
       gender: ['Male']
-    })
+    });
   }
 
    /* Add dynamic languages */
@@ -61,11 +61,42 @@ export class AddStudentComponent implements OnInit {
     const value = event.value;
     // Add language
     if ((value || '').trim() && this.subjectArray.length < 5) {
-      this.subjectArray.push({ name: value.trim() })
+      this.subjectArray.push({ name: value.trim() });
     }
     // Reset the input value
     if (input) {
       input.value = '';
     }
    }
+
+     /* Remove dynamic languages */
+  remove(subject: Subject): void {
+    const index = this.subjectArray.indexOf(subject);
+    if (index >= 0) {
+      this.subjectArray.splice(index, 1);
+    }
+  }
+
+ /* Date */
+ formatDate(e): void {
+  let convertDate = new Date(e.target.value).toISOString().substring(0, 10);
+  this.studentForm.get('dob').setValue(convertDate, {
+    onlyself: true
+  });
+}
+
+/* Get errors */
+  public handleError = (controlName: string, errorName: string) => {
+    return this.studentForm.controls[controlName].hasError(errorName);
+  }
+
+/* Submit book */
+submitStudentForm(): void {
+  if (this.studentForm.valid) {
+    this.studentApi.AddStudent(this.studentForm.value).subscribe( res => {
+      this.ngZone.run(() => this.router.navigateByUrl('/students-list'));
+    });
+  }
+}
+
 }
